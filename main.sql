edadeb448b0d9222f6ab9c5a78473855f5083021
marlonigor <66376336+marlonigor@users.noreply.github.com>
2023-03-05 22:12:40 -0300

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,3 +1,11 @@
+/*The task is to write a SQL query to concatenate firstname and lastname columns into one column named 'fullname', 
+and calculate the total annual salary for each employee, 
+including bonuses, 
+and display it in a column named 'total', 
+sorted in ascending order by the 'total' column.
+*/
+
+
 SELECT CONCAT(firstname,' ', lastname) AS fullname, salary*12+(500*experience) AS total
 FROM staff
 ORDER BY total;
